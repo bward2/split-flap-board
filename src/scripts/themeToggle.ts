4b15a404f178db8571e42a01e72622c516c4b0d1
bward2 <d3wardbrent@gmail.com>
2022-07-29 21:18:05 -0500

    feat: Hooks up toggle switch to save user preference for color scheme.

--- src/scripts/themeToggle.ts
+++ src/scripts/themeToggle.ts
@@ -1,11 +1,26 @@
+const themeSwitch = document.getElementById('theme-switch') as HTMLInputElement;
+const enum themes {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+
 const loadTheme = () => {
   const storedTheme =
-    localStorage.getItem('theme') ||
-    window.matchMedia('(prefers-color-scheme: light)').matches
-      ? 'light'
-      : 'dark';
+    localStorage.getItem('data-theme') ||
+    (window.matchMedia('(prefers-color-scheme: light)').matches
+      ? themes.LIGHT
+      : themes.DARK);
 
   document.documentElement.setAttribute('data-theme', storedTheme);
+  themeSwitch.checked = storedTheme === themes.DARK;
+};
+
+themeSwitch.onclick = () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === themes.LIGHT ? themes.DARK : themes.LIGHT;
+
+  document.documentElement.setAttribute('data-theme', newTheme);
+  localStorage.setItem('data-theme', newTheme);
 };
 
 export default loadTheme;
