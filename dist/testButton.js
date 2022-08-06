0e049e2992b940c2a132a3249262f9bb13702d0c
bward2 <d3wardbrent@gmail.com>
2022-08-05 22:01:04 -0500

    feat: Hooks up basic logic for manually flipping a panel.

--- dist/testButton.js
+++ dist/testButton.js
@@ -1,10 +1,27 @@
 "use strict";
 var testButton = document.getElementById('test-button');
+var topFullFlaps = document.querySelectorAll('.top-full');
 var topHalfFlaps = document.querySelectorAll('.top-half');
 var bottomHalfFlaps = document.querySelectorAll('.bottom-half');
+var bottomFullFlaps = document.querySelectorAll('.bottom-full');
+var panelCharacters = ['', 'A', 'B', 'C', '1', '2', '3'];
 testButton.onclick = function () {
-    for (var index = 0; index < topHalfFlaps.length; index++) {
+    var _loop_1 = function (index) {
         topHalfFlaps[index].classList.add('top-half-flip');
         bottomHalfFlaps[index].classList.add('bottom-half-flip');
+        setTimeout(function () {
+            var current = panelCharacters.indexOf(topFullFlaps[index].innerHTML);
+            var next = current === panelCharacters.length - 1 ? 0 : current + 1;
+            console.log(current, next);
+            topFullFlaps[index].innerHTML = panelCharacters[next];
+            topHalfFlaps[index].innerHTML = panelCharacters[current];
+            bottomHalfFlaps[index].innerHTML = panelCharacters[next];
+            bottomFullFlaps[index].innerHTML = panelCharacters[current];
+            topHalfFlaps[index].classList.remove('top-half-flip');
+            bottomHalfFlaps[index].classList.remove('bottom-half-flip');
+        }, 400);
+    };
+    for (var index = 0; index < topHalfFlaps.length; index++) {
+        _loop_1(index);
     }
 };
--- src/scripts/testButton.ts
+++ src/scripts/testButton.ts
@@ -1,11 +1,30 @@
 const testButton = document.getElementById('test-button') as HTMLElement;
 
+const topFullFlaps = document.querySelectorAll('.top-full');
 const topHalfFlaps = document.querySelectorAll('.top-half');
 const bottomHalfFlaps = document.querySelectorAll('.bottom-half');
+const bottomFullFlaps = document.querySelectorAll('.bottom-full');
+
+const panelCharacters = ['', 'A', 'B', 'C', '1', '2', '3'];
 
 testButton.onclick = () => {
   for (let index = 0; index < topHalfFlaps.length; index++) {
     topHalfFlaps[index].classList.add('top-half-flip');
     bottomHalfFlaps[index].classList.add('bottom-half-flip');
+
+    setTimeout(() => {
+      const current = panelCharacters.indexOf(topFullFlaps[index].innerHTML);
+      const next = current === panelCharacters.length - 1 ? 0 : current + 1;
+
+      console.log(current, next);
+
+      topFullFlaps[index].innerHTML = panelCharacters[next];
+      topHalfFlaps[index].innerHTML = panelCharacters[current];
+      bottomHalfFlaps[index].innerHTML = panelCharacters[next];
+      bottomFullFlaps[index].innerHTML = panelCharacters[current];
+
+      topHalfFlaps[index].classList.remove('top-half-flip');
+      bottomHalfFlaps[index].classList.remove('bottom-half-flip');
+    }, 400);
   }
 };
