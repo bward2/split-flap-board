4029a0d02855991cc07a69bf7be4de3f118f867a
bward2 <d3wardbrent@gmail.com>
2022-08-14 21:54:27 -0500

    chore: Adds major refactor to modularize code.

--- src/scripts/testButton.js
+++ src/scripts/testButton.js
@@ -1,13 +1,17 @@
-import { panelCharacters, flipSpeed } from './constants.js';
+import { getBoardTarget, setBoardTarget } from './boardManager.js';
+import {
+  topFullFlaps,
+  topHalfFlaps,
+  bottomHalfFlaps,
+  bottomFullFlaps,
+  testButtonFlip,
+  testButtonReset,
+  panelCharacters,
+  flipSpeed,
+  sounds,
+} from './constants.js';
+import { flipPanel } from './panelManager.js';
 import { playSound } from './soundManager.js';
-
-const testButtonFlip = document.getElementById('test-button-flip');
-const testButtonReset = document.getElementById('test-button-reset');
-
-const topFullFlaps = document.querySelectorAll('.top-full');
-const topHalfFlaps = document.querySelectorAll('.top-half');
-const bottomHalfFlaps = document.querySelectorAll('.bottom-half');
-const bottomFullFlaps = document.querySelectorAll('.bottom-full');
 
 for (let index = 0; index < topFullFlaps.length; index++) {
   const topFullFlap = topFullFlaps[index];
@@ -21,75 +25,28 @@ for (let index = 0; index < topFullFlaps.length; index++) {
   bottomFullFlap.style.animationDuration = `${flipSpeed}s`;
 }
 
-/**
- *
- * @param {Boolean} flipOnce
- */
-const flipThem = (flipOnce) => {
+const flipThem = () => {
   for (let index = 0; index < topFullFlaps.length; index++) {
-    const topFullFlap = topFullFlaps[index];
-    const topHalfFlap = topHalfFlaps[index];
-    const bottomHalfFlap = bottomHalfFlaps[index];
-    const bottomFullFlap = bottomFullFlaps[index];
-
-    topFullFlaps[index].classList.remove('top-full-slide');
-    topHalfFlaps[index].classList.remove('top-half-flip');
-    bottomHalfFlaps[index].classList.remove('bottom-half-flip');
-
-    void topFullFlap.offsetWidth;
-    void topHalfFlap.offsetWidth;
-    void bottomHalfFlap.offsetWidth;
-
-    topFullFlaps[index].classList.add('top-full-slide');
-    topHalfFlaps[index].classList.add('top-half-flip');
-    bottomHalfFlaps[index].classList.add('bottom-half-flip');
-
-    playSound('flip');
-
-    setTimeout(() => {
-      playSound('flap');
-
-      const current = panelCharacters.indexOf(topFullFlaps[index].innerHTML);
-      const next = current === panelCharacters.length - 1 ? 0 : current + 1;
-
-      topFullFlaps[index].innerHTML = panelCharacters[next];
-      topHalfFlaps[index].innerHTML = panelCharacters[current];
-      bottomHalfFlaps[index].innerHTML = panelCharacters[next];
-      bottomFullFlaps[index].innerHTML = panelCharacters[current];
-
-      topFullFlaps[index].classList.remove('top-full-slide');
-      topHalfFlaps[index].classList.remove('top-half-flip');
-      bottomHalfFlaps[index].classList.remove('bottom-half-flip');
-
-      if (flipOnce) {
-        testButtonFlip.disabled = false;
-
-        bottomFullFlaps[index].classList.remove('bottom-full-bounce');
-        void bottomFullFlap.offsetWidth;
-        bottomFullFlaps[index].classList.add('bottom-full-bounce');
-      } else {
-        if (current === 0) {
-          testButtonFlip.disabled = false;
-          testButtonReset.disabled = false;
-
-          bottomFullFlaps[index].classList.remove('bottom-full-bounce');
-          void bottomFullFlap.offsetWidth;
-          bottomFullFlaps[index].classList.add('bottom-full-bounce');
-        } else {
-          flipThem(false);
-        }
-      }
-    }, flipSpeed * 1000);
+    flipPanel(index);
   }
 };
 
 testButtonFlip.onclick = () => {
   testButtonFlip.disabled = true;
-  flipThem(true);
+  testButtonReset.disabled = true;
+
+  const currentIndex = panelCharacters.indexOf(getBoardTarget()[0]);
+  const targetIndex =
+    currentIndex === panelCharacters.length - 1 ? 0 : currentIndex + 1;
+
+  setBoardTarget(panelCharacters[targetIndex]);
+  flipThem();
 };
 
 testButtonReset.onclick = () => {
   testButtonFlip.disabled = true;
   testButtonReset.disabled = true;
-  flipThem(false);
+
+  setBoardTarget(panelCharacters[0]);
+  flipThem();
 };
