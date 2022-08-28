ca5ad93f8d00cb8524081fd8678305e56bcd2e1a
bward2 <d3wardbrent@gmail.com>
2022-08-27 22:25:22 -0500

    feat: Adds back animations.

--- src/scripts/boardManager.js
+++ src/scripts/boardManager.js
@@ -1,6 +1,8 @@
 import {
+  animations,
   boardColumns,
   boardRows,
+  flipSpeed,
   panelCharacters,
   splitFlapBoard,
 } from './constants.js';
@@ -23,17 +25,37 @@ const getPanelHtml = (panelIndex) => {
     nextIndex = 0;
   }
 
-  return `
-  <div class="split-flap-panel">
-    <div class="new-card-overlay-top"></div>
-    <div class="top-new">${panelCharacters[currentIndex]}</div>
-    <div class="top-current">${panelCharacters[currentIndex]}</div>
-    <div class="split-flap-divider"></div>
-    <div class="bottom-new">${panelCharacters[nextIndex]}</div>
-    <div class="bottom-current">${panelCharacters[currentIndex]}</div>
-    <div class="new-card-overlay-bottom"></div>
-  </div>
-`;
+  let panelHtml;
+
+  if (
+    getBoardStatus()[panelIndex].target !== getBoardStatus()[panelIndex].actual
+  ) {
+    panelHtml = `
+      <div class="split-flap-panel">
+        <div class="new-card-overlay-top"></div>
+        <div class="top-new ${animations.TOP_NEW_SLIDE}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[nextIndex]}</div>
+        <div class="top-current ${animations.TOP_CURRENT_FLIP}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[currentIndex]}</div>
+        <div class="split-flap-divider"></div>
+        <div class="bottom-new ${animations.BOTTOM_NEW_FLIP}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[nextIndex]}</div>
+        <div class="bottom-current ${animations.BOTTOM_CURRENT_SLIDE}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[currentIndex]}</div>
+        <div class="new-card-overlay-bottom"></div>
+      </div>
+    `;
+  } else {
+    panelHtml = `
+      <div class="split-flap-panel">
+        <div class="new-card-overlay-top"></div>
+        <div class="top-new">${panelCharacters[nextIndex]}</div>
+        <div class="top-current">${panelCharacters[currentIndex]}</div>
+        <div class="split-flap-divider"></div>
+        <div class="bottom-new">${panelCharacters[nextIndex]}</div>
+        <div class="bottom-current">${panelCharacters[currentIndex]}</div>
+        <div class="new-card-overlay-bottom"></div>
+      </div>
+    `;
+  }
+
+  return panelHtml;
 };
 
 export const redrawBoard = () => {
@@ -63,7 +85,7 @@ const setup = () => {
     for (let currentCol = 0; currentCol < boardColumns; currentCol += 1) {
       const index = currentRow * boardColumns + currentCol;
       boardStatus[index] = {
-        current: 0,
+        actual: 0,
         target: 0,
         inMotion: false,
       };
