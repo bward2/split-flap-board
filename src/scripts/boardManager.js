794bb18c24600dbf3c4a0082b56b274e2ea0ae4f
bward2 <d3wardbrent@gmail.com>
2022-09-06 14:35:02 -0500

    feat: Begins major rehaul of animations.

--- src/scripts/boardManager.js
+++ src/scripts/boardManager.js
@@ -1,104 +1,60 @@
-import {
-  animations,
-  boardColumns,
-  boardRows,
-  flipSpeed,
-  panelCharacters,
-  splitFlapBoard,
-} from './constants.js';
-import { playSound } from './soundManager.js';
+import { boardColumns, boardRows, panelCharacters } from './constants.js';
 
-let boardStatus = [];
+const topFlaps = [];
+const bottomFlaps = [];
+const topAnimations = [];
+const bottomAnimations = [];
 
-export const setBoardStatus = (newTarget) => {
-  boardStatus = newTarget;
+const createTopFlap = () => {
+  const newTopFlap = document.createElement('div');
+  newTopFlap.innerText = panelCharacters[1];
+  newTopFlap.classList.add('top-flap');
+
+  topFlaps.push(newTopFlap);
+
+  return newTopFlap;
 };
 
-export const getBoardStatus = () => {
-  return boardStatus;
+const createDivider = () => {
+  const newDividerDiv = document.createElement('div');
+  newDividerDiv.classList.add('split-flap-divider');
+
+  return newDividerDiv;
 };
 
-const getPanelHtml = (panelIndex) => {
-  const currentIndex = getBoardStatus()[panelIndex].actual;
-  let nextIndex = currentIndex + 1;
+const createBottomFlap = () => {
+  const newBottomFlap = document.createElement('div');
+  newBottomFlap.innerText = panelCharacters[1];
+  newBottomFlap.classList.add('bottom-flap');
 
-  if (nextIndex === panelCharacters.length) {
-    nextIndex = 0;
-  }
+  bottomFlaps.push(newBottomFlap);
 
-  let panelHtml;
-
-  if (
-    getBoardStatus()[panelIndex].target !== getBoardStatus()[panelIndex].actual
-  ) {
-    panelHtml = `
-      <div class="split-flap-panel">
-        <div class="new-card-overlay-top"></div>
-        <div class="top-new ${animations.TOP_NEW_SLIDE}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[nextIndex]}</div>
-        <div class="top-current ${animations.TOP_CURRENT_FLIP}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[currentIndex]}</div>
-        <div class="split-flap-divider"></div>
-        <div class="bottom-new ${animations.BOTTOM_NEW_FLIP}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[nextIndex]}</div>
-        <div class="bottom-current ${animations.BOTTOM_CURRENT_SLIDE}" style="animation-duration: ${flipSpeed}s;">${panelCharacters[currentIndex]}</div>
-        <div class="new-card-overlay-bottom"></div>
-      </div>
-    `;
-
-    // TODO: Put this elsewhere
-    boardStatus[panelIndex].actual = nextIndex;
-  } else {
-    panelHtml = `
-      <div class="split-flap-panel">
-        <div class="new-card-overlay-top"></div>
-        <div class="top-new">${panelCharacters[nextIndex]}</div>
-        <div class="top-current">${panelCharacters[currentIndex]}</div>
-        <div class="split-flap-divider"></div>
-        <div class="bottom-new">${panelCharacters[nextIndex]}</div>
-        <div class="bottom-current">${panelCharacters[currentIndex]}</div>
-        <div class="new-card-overlay-bottom"></div>
-      </div>
-    `;
-  }
-
-  return panelHtml;
-};
-
-export const redrawBoard = () => {
-  let newInnerHtml = '';
-
-  playSound('flip');
-
-  for (let currentRow = 0; currentRow < boardRows; currentRow += 1) {
-    for (let currentCol = 0; currentCol < boardColumns; currentCol += 1) {
-      if (currentCol === 0) {
-        newInnerHtml = `${newInnerHtml} <div class="split-flap-row">`;
-      }
-
-      const panelIndex = currentRow * boardColumns + currentCol;
-      const panelHtml = getPanelHtml(panelIndex);
-      newInnerHtml = `${newInnerHtml} ${panelHtml}`;
-
-      if (currentCol === boardColumns - 1) {
-        newInnerHtml = `${newInnerHtml} </div>`;
-      }
-    }
-  }
-
-  splitFlapBoard.innerHTML = newInnerHtml;
+  return newBottomFlap;
 };
 
 const setup = () => {
-  for (let currentRow = 0; currentRow < boardRows; currentRow += 1) {
-    for (let currentCol = 0; currentCol < boardColumns; currentCol += 1) {
-      const index = currentRow * boardColumns + currentCol;
-      boardStatus[index] = {
-        actual: 0,
-        target: 0,
-        inMotion: false,
-      };
-    }
-  }
+  const splitFlapBoard = document.getElementById('split-flap-board');
 
-  redrawBoard();
+  for (let row = 0; row < boardRows; row += 1) {
+    const newRowDiv = document.createElement('div');
+    newRowDiv.classList.add('split-flap-row');
+
+    for (let column = 0; column < boardColumns; column += 1) {
+      const newPanelDiv = document.createElement('div');
+      newPanelDiv.classList.add('split-flap-panel');
+
+      const newTopFlap = createTopFlap();
+      const newDividerDiv = createDivider();
+      const newBottomFlap = createBottomFlap();
+
+      newPanelDiv.appendChild(newTopFlap);
+      newPanelDiv.appendChild(newDividerDiv);
+      newPanelDiv.appendChild(newBottomFlap);
+      newRowDiv.appendChild(newPanelDiv);
+    }
+
+    splitFlapBoard.appendChild(newRowDiv);
+  }
 };
 
 setup();
--- src/scripts/testButton.js
+++ src/scripts/testButton.js
@@ -1,41 +1,9 @@
-import { getBoardStatus, redrawBoard, setBoardStatus } from './boardManager.js';
-import {
-  testButtonFlip,
-  testButtonReset,
-  panelCharacters,
-  flipSpeed,
-} from './constants.js';
+import { testButtonFlip, testButtonReset } from './constants.js';
 
 testButtonFlip.onclick = () => {
-  let newBoardStatus = getBoardStatus();
-
-  for (let index = 0; index < getBoardStatus().length; index += 1) {
-    const newTarget =
-      newBoardStatus[index].target === panelCharacters.length - 1
-        ? 0
-        : newBoardStatus[index].target + 1;
-    newBoardStatus[index].target = newTarget;
-  }
-
-  setBoardStatus(newBoardStatus);
-  redrawBoard();
+  console.log('Flip!');
 };
 
 testButtonReset.onclick = () => {
-  let redrawInterval;
-  let newBoardStatus = getBoardStatus();
-
-  for (let index = 0; index < getBoardStatus().length; index += 1) {
-    newBoardStatus[index].target = 0;
-  }
-
-  setBoardStatus(newBoardStatus);
-
-  redrawInterval = setInterval(() => {
-    if (getBoardStatus()[0].actual === getBoardStatus()[0].target) {
-      clearInterval(redrawInterval);
-      return;
-    }
-    redrawBoard();
-  }, flipSpeed * 1000);
+  console.log('Reset!');
 };
