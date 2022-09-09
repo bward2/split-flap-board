b7ee8d078bb3fa8f82cd64dee33e16b3e8c7ff19
bward2 <d3wardbrent@gmail.com>
2022-09-09 13:03:27 -0500

    feat: Deletes EVERYTHING in preparation of a new animation method...

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,19 +1,5 @@
 export const splitFlapBoard = document.getElementById('split-flap-board');
 
-export const getFlapSelectors = () => {
-  const topNewFlaps = document.querySelectorAll('.top-new');
-  const topCurrentFlaps = document.querySelectorAll('.top-current');
-  const bottomNewFlaps = document.querySelectorAll('.bottom-new');
-  const bottomCurrentFlaps = document.querySelectorAll('.bottom-current');
-
-  return {
-    topNewFlaps,
-    topCurrentFlaps,
-    bottomNewFlaps,
-    bottomCurrentFlaps,
-  };
-};
-
 export const testButtonFlip = document.getElementById('test-button-flip');
 export const testButtonReset = document.getElementById('test-button-reset');
 
@@ -66,16 +52,3 @@ export const sounds = {
   FLIP: 'flip',
   FLAP: 'flap',
 };
-
-export const animations = {
-  TOP_NEW_SLIDE: 'top-new-slide',
-  TOP_CURRENT_FLIP: 'top-current-flip',
-  BOTTOM_NEW_FLIP: 'bottom-new-flip',
-  BOTTOM_CURRENT_SLIDE: 'bottom-current-slide',
-  BOTTOM_CURRENT_BOUNCE: 'bottom-current-bounce',
-};
-
-export const splitPanelHeight = `calc((90vw / ${boardColumns}) * 0.85)`;
-export const splitPanelDividerHeight = 'calc(var(--split-panel-height) * 0.05)';
-export const halfSplitPanelDividerHeight =
-  'calc(var(--split-panel-divider-height) / 2)';
--- src/scripts/testButton.js
+++ src/scripts/testButton.js
@@ -1,17 +1,6 @@
-import { bottomAnimations, topAnimations } from './boardManager.js';
 import { testButtonFlip, testButtonReset } from './constants.js';
-
-let blah = 0;
-
 testButtonFlip.onclick = () => {
-  topAnimations.forEach((animation) => {
-    animation[blah].play();
-  });
-  bottomAnimations.forEach((animation) => {
-    animation[blah].play();
-  });
-
-  blah += 1;
+  console.log('Test!');
 };
 
 testButtonReset.onclick = () => {
