2e68138ede6680c8c77bd6a0a9cf4b6ddd03f282
bward2 <d3wardbrent@gmail.com>
2022-09-09 22:14:44 -0500

    feat: Hooks up simple test for animating a really huge PNG.

--- src/scripts/testButton.js
+++ src/scripts/testButton.js
@@ -1,6 +1,22 @@
-import { testButtonFlip, testButtonReset } from './constants.js';
+import { sounds, testButtonFlip, testButtonReset } from './constants.js';
+import { playSound } from './soundManager.js';
+
+const animatePanel = (panel, index) => {
+  const animation = panel.animate(
+    [{ transform: 'translateX(0)' }, { transform: 'translateX(-900px)' }],
+    {
+      fill: 'forwards',
+      easing: 'steps(9)',
+      duration: 1000,
+    }
+  );
+};
+
 testButtonFlip.onclick = () => {
-  console.log('Test!');
+  const panels = document.querySelectorAll('.split-flap-panel');
+  panels.forEach((panel, index) => {
+    animatePanel(panel, index);
+  });
 };
 
 testButtonReset.onclick = () => {
