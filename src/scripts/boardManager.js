3c1c9775ce911d917b693f3956ebc5535edc019d
bward2 <d3wardbrent@gmail.com>
2022-10-02 19:25:46 -0500

    chore: Refactors pageManager to have a boardManager for handling panel tasks.

--- src/scripts/boardManager.js
+++ src/scripts/boardManager.js
@@ -1,1 +1,57 @@
-// TODO
+import { boardColumns, boardRows } from './constants.js';
+import { PanelManager } from './panelManager.js';
+
+export class BoardManager {
+  constructor(panelSize, theme) {
+    this.panels = [];
+    this.panelSize = panelSize;
+    this.theme = theme;
+
+    const board = document.getElementById('split-flap-board');
+    for (let rowIndex = 0; rowIndex < boardRows; rowIndex += 1) {
+      let newBoardRow = document.createElement('div');
+      newBoardRow.classList.add('split-flap-row');
+
+      for (let columnIndex = 0; columnIndex < boardColumns; columnIndex += 1) {
+        const panel = new PanelManager(this.panelSize, this.theme);
+        this.panels.push(panel);
+        newBoardRow.appendChild(panel.getContainer());
+      }
+
+      board.appendChild(newBoardRow);
+    }
+  }
+
+  getPanels() {
+    return this.panels;
+  }
+
+  setTheme(theme) {
+    this.theme = theme;
+
+    this.panels.forEach((panel) => {
+      panel.setTheme(this.theme);
+    });
+  }
+
+  setPanelSize(newPanelSize) {
+    this.panelSize = newPanelSize;
+
+    this.panels.forEach((panel) => {
+      panel.setPanelSize(this.panelSize);
+    });
+  }
+
+  //TODO: Remove these test methods once they are no longer needed
+  flipAllPanels() {
+    this.panels.forEach((panel) => {
+      panel.flip();
+    });
+  }
+
+  resetAllPanels() {
+    this.panels.forEach((panel) => {
+      panel.reset();
+    });
+  }
+}
